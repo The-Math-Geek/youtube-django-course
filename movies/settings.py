32b8499a4f417aceb87d958ac044439218b78fac
The-Math-Geek <TheMathGeek@techie.com>
2023-02-18 17:39:58 -0500

    Create Delete functionality added Error 404

--- movies/settings.py
+++ movies/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--ii%_dllr$(d+1s745iv(4$h_orcz55m_wq%myxqol6hnxdxka'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
--- movies/views.py
+++ movies/views.py
@@ -1,35 +1,38 @@
-from django.http import HttpResponse;
+from django.http import HttpResponse, HttpResponseRedirect, Http404;
 from django.shortcuts import render;
-
-data={
-    "movies": [
-    {
-        "id": 5,
-        "title": "Jaws",
-        "year": 1969,
-    },
-
-    {
-        "id": 6,
-        "title": "Sharknado",
-        "year": 1600,
-    },
-
-    {
-        "id": 7,
-        "title": "The Meg",
-        "year": 2000,
-    },]
-
-    }
+from .models import Movie
 
 
 def movies(request):
-    return render(request, "movies/movies.html", data)
+    data = Movie.objects.all()
+    return render(request, "movies/movies.html", {"movies": data})
     """ For above render statement...""" 
     """First Argument = request""" 
     """Second Argument = Template (Must include navigation for the template this render should be pulling from. i.e. within templates folder, go to movies folder, then movies.html doc"""
     """Third Argument = Data that is passed to the template in order to be rendered. 'movies' is the variable that will be set in double curly braces in the movies.html file. Everything in the square brackets--including the square brackets--will be rendered onto the website screen!"""
 
 def home(request):
-    return HttpResponse("Home Page")
+    return HttpResponse("Home page")
+
+def detail(request, id):
+    data = Movie.objects.get(pk=id)
+    return render(request, "movies/detail.html", {"movie": data})
+
+def add(request):
+    title = request.POST.get("title")
+    year = request.POST.get("year")
+
+    if title and year:
+        movie = Movie(title=title, year=year)
+        movie.save()
+        return HttpResponseRedirect("/movies")
+
+    return render(request, "movies/add.html")
+
+def delete(request, id):
+    try:
+        movie = Movie.objects.get(pk=id)
+    except:
+        raise Http404("Hmmm... The movie you attempted to view does not seem to exist. Please try again, or contact your system administrator.")
+    movie.delete()
+    return HttpResponseRedirect("/movies")
